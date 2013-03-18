4019b185d49e56b45d90cce3fb19965e6f4af51b
Tsachi Shlidor <tsi.shlidor@gmail.com>
2013-03-18 05:18:18 -0700

    Updated sprint.js

--- sprint.js
+++ sprint.js
@@ -1,47 +1,73 @@
 (function($) {
-  
+
+// === S(imple)Print ===
+//
+// This script will create a printable version of a page.
+//
+// Usage:
+// a. Add a link to your page :
+//
+//    <a href="#" class="sprint" data-sprint="#content">PRINT</a>
+//
+//    note: data-sprint attribute is a selector for your printable content.
+//          defaults to '#page'.
+//
+// b. Call it directly:
+//
+//    $('#page').sprint();
+
+  $.fn.extend({
+    sprint: function() {
+
+      // Add 'print' class for easier print styling.
+      $("body").addClass("print");
+
+      // Printable content.
+      var printContent = $(document.createElement('div'));
+      $(this).each(function() {
+        $(this).appendTo(printContent);
+      });
+      if (printContent.length) {
+        $('body').html(printContent);
+      }
+
+      // Add top toolbar.
+      var bar = $('<div class="simple-print-bar" />')
+        .append('<a href="#" class="print">' + Drupal.t('Send to printer') + '</a> ')
+        .append('<a href="#" class="back">' + Drupal.t('Back to original post') + '</a> ')
+        .hide().prependTo('body').slideDown();
+      
+      $('html, body').animate({scrollTop:'0'}, 500);
+
+      // We simply refresh to go back to the normal view.
+      $('.simple-print-bar a.back').click(function() {
+        location.reload();
+      });
+
+      // send-to-printer link.
+      $('.simple-print-bar a.print').click(function() {
+        text=document;
+        print(text);
+      });
+
+    }
+  });
+
+
   Drupal.behaviors.simplePrint = {
     attach: function(context) {
 
-      $('a.download').click(function(e) { // the link selector should be exposed to the user.
+      $('a.sprint').click(function(e) {
 
-        // Prevent the link behaviour
+        // Prevent the link behaviour.
         e.preventDefault();
 
-        // Remove the link
-        $(this).remove();
-
-        // Add "print" body class so themes can easily add css rules to this process.
-        $("body").addClass("print");
-
-        // Remove everything but #page.
-        $('body').html(
-          $('#page').html() // todo: We should expose these selectors to the user.
-        );
-
-        // Add top toolbar.
-        var bar = $('<div class="simple-print-bar" />')
-          .prependTo('body')
-          .append('<a href="#" class="print">' + Drupal.t('Send to printer') + '</a> ')
-          .append('<a href="#" class="back">' + Drupal.t('Back to original post') + '</a> ');  
-        
-        $('html, body').animate({scrollTop:'0'}, 500);
-
-        // We simply refresh to go back to the normal view.
-        $('.simple-print-bar a.back').click(function() {
-          location.reload();
-        });
-
-        // send-to-printer link
-        $('.simple-print-bar a.print').click(function() {
-          text=document;
-          print(text);
-        });
+        // The 'data-sprint' attribute can set the printable content wrapper.
+        var wrap = $(this).attr('data-sprint') || '#page';
+        $(wrap).sprint();
 
       });
 
     }
-  
   };
-  
 })(jQuery);
